bd5d1d7e808cf1776db6da49bd5807df9a6d7dc5
Thaman Gurung <tgurung519@gmail.com>
2025-12-04 10:55:30 +0545

    initial setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
 
   return (
-    <div> 
-      Hello
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+      </Routes>
+    </Router>
   )
 }
 
--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import dotenv from 'dotenv';
+import { connectDB } from './configs/db.ts';
+dotenv.config();
 const app = express();
+connectDB();
+
 app.get("/", function(req, res){
     return res.send("Hello World!");
 })
 
-app.listen(3000, () => {
-    console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server started on port "+ process.env.PORT);
 });
